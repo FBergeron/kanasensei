8420a8932f03fb756cb0b1187ed83b45aeee567d
Frederic Bergeron <fbergeron@users.sourceforge.net>
2002-05-04 12:11:05 +0000

    getImageResourceFile now supports png and jpg files; loadImages() and copy() methods added.

--- src/com/fbergeron/util/Util.java
+++ src/com/fbergeron/util/Util.java
@@ -25,18 +25,80 @@ import java.util.*;
 
 import com.sixlegs.image.png.PngImage;
 
+/** 
+ * Class containing miscellaneous utility functions.
+ */
 public class Util {
+    /** 
+     * Gets an image resource. 
+     * @param strResourceFilename Name of the image file.
+     * @param srcClass Class from which the location of the resource is determined.
+     * @return Instance of an <code>Image</code>.
+     */
     public static Image getImageResourceFile( String strResourceFilename, Class srcClass ) {
         PngImage pngImage = null;
         Image image = null;
-        BufferedInputStream in = new BufferedInputStream(
-            srcClass.getResourceAsStream( strResourceFilename ) );
-        if( in == null ) {
-            System.err.println( "Image not found:" + strResourceFilename );
-            return null;
+        try {
+            BufferedInputStream in = new BufferedInputStream(
+                srcClass.getResourceAsStream( strResourceFilename ) );
+            if( in == null ) {
+                System.err.println( "Image not found:" + strResourceFilename );
+                return null;
+            }
+            if( strResourceFilename.endsWith( ".png" ) ) {
+                pngImage = new PngImage( in );
+                image = Toolkit.getDefaultToolkit().createImage( pngImage );
+            }
+            else { 
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                copy( in, out );
+                image = Toolkit.getDefaultToolkit().createImage( out.toByteArray() );
+            }
         }
-        pngImage = new PngImage( in );
-        image = Toolkit.getDefaultToolkit().createImage( pngImage );
+        catch( java.io.IOException e ) {
+            System.err.println( "Unable to read image " + strResourceFilename + "." );
+            e.printStackTrace();
+        }
 	    return( image );
 	}
+    
+	/** 
+	 * Load images (used for preloading images).
+	 * @param images Array of <code>Image</code> instances to preload.
+	 * @param comp Component that will observe the loading state of the images.
+	 */
+	public static void loadImages( Image[] images, Component comp ) {
+		MediaTracker tracker = new MediaTracker( comp );
+		for( int i = 0; i < images.length; i++ )
+			tracker.addImage( images[ i ], 0 );
+		try {
+			tracker.waitForID( 0 );
+		}
+		catch( InterruptedException ignore ) {
+		}
+    }
+    
+    /** 
+     * Copy data from an input stream to an output stream. 
+     * @param in Input stream.
+     * @param out Output stream.
+     * @throws IOException 
+     */
+    public static void copy( InputStream in, OutputStream out ) throws IOException
+    {
+        // Do not allow other threads to read from the
+        // input or write to the output while copying is
+        // taking place
+        synchronized( in ) {
+            synchronized( out ) {
+                byte[] buffer = new byte[ 1024 ];
+                while( true ) {
+                    int bytesRead = in.read( buffer );
+                    if( bytesRead == -1 )
+                    	break;
+                    out.write( buffer, 0, bytesRead );
+                }
+            }
+        }
+    }
 }
