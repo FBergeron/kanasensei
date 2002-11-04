d6dc5e0440d8cf3eeca254c7450bc33c54d98a3a
Frederic Bergeron <fbergeron@users.sourceforge.net>
2002-11-04 05:58:20 +0000

    I've added setMessage() method.  It's needed to change the message when the locale changes.

--- src/com/fbergeron/util/DialogMsg.java
+++ src/com/fbergeron/util/DialogMsg.java
@@ -23,43 +23,47 @@ import java.awt.event.*;
 
 public class DialogMsg extends Dialog
 {
-	/**
-	 * Simple dialog that displays a message.
-	 * 
-	 * @param parent Parent frame from which the dialog is invoked.
-	 * @param title Title of the dialog window.
-	 * @param modal <code>true</code>, if it's a modal dialog.
-	 * @param strMsg Message displayed in the dialog window.
-	 */
-	public DialogMsg(Frame parent, String title, boolean modal, String strMsg)
-	{
-		super(parent, modal);
+    /**
+     * Simple dialog that displays a message.
+     * 
+     * @param parent Parent frame from which the dialog is invoked.
+     * @param title Title of the dialog window.
+     * @param modal <code>true</code>, if it's a modal dialog.
+     * @param strMsg Message displayed in the dialog window.
+     */
+    public DialogMsg(Frame parent, String title, boolean modal, String strMsg)
+    {
+        super(parent, modal);
 
-		setLayout(new BorderLayout(0,0));
-		setVisible(false);
-		setSize(240,110);
-		setBackground(new Color(12632256));
-		
-		panelBackground = new java.awt.Panel();
-		panelBackground.setLayout(new BorderLayout(0,0));
-		panelBackground.setBackground(Color.white);
-		add("Center", panelBackground);
-		
-		textAreaMsg = new java.awt.TextArea("",0,0,TextArea.SCROLLBARS_VERTICAL_ONLY);
-		textAreaMsg.setEditable(false);
-	    textAreaMsg.setText(strMsg);
-		//textAreaMsg.setBackground(new Color(16777199));
-		panelBackground.add("Center",textAreaMsg);
+        setLayout(new BorderLayout(0,0));
+        setVisible(false);
+        setSize(240,110);
+        setBackground(new Color(12632256));
+        
+        panelBackground = new java.awt.Panel();
+        panelBackground.setLayout(new BorderLayout(0,0));
+        panelBackground.setBackground(Color.white);
+        add("Center", panelBackground);
+        
+        textAreaMsg = new java.awt.TextArea("",0,0,TextArea.SCROLLBARS_VERTICAL_ONLY);
+        textAreaMsg.setEditable(false);
+        textAreaMsg.setText(strMsg);
+        //textAreaMsg.setBackground(new Color(16777199));
+        panelBackground.add("Center",textAreaMsg);
 
-		setTitle(title);
-		setResizable(false);
-		
+        setTitle(title);
+        setResizable(false);
+        
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation((d.width - getSize().width)/2, (d.height - getSize().height)/2);
 
-		addWindowListener( new WindowManager( this, WindowManager.DISPOSE_ON_CLOSE ) );
-	}
+        addWindowListener( new WindowManager( this, WindowManager.DISPOSE_ON_CLOSE ) );
+    }
 
-	Panel panelBackground;
-	TextArea textAreaMsg;
+    public void setMessage( String message ) {
+        textAreaMsg.setText( message );
+    }
+
+    Panel panelBackground;
+    TextArea textAreaMsg;
 }
